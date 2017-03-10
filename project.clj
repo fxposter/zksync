87b77175503068bd56b1160d75687054cdca2ab2
Pavel Forkert <fxposter@gmail.com>
2017-03-10 17:11:51 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zksync "0.1.0-SNAPSHOT"
+(defproject zksync "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
