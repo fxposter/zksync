d806004c916fc26a538ba0b6608f5ffd5c23ce13
Pavel Forkert <fxposter@gmail.com>
2017-03-10 17:13:07 +0200

    Description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zksync "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Syncing zookeeper subtrees between multiple clusters"
+  :url "https://github.com/fxposter/zksync"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
